55f32c8f0ec4ad937dd224dbc9fd3ca17c95b284
Edwin Onuonga <edwinonuonga@gmail.com>
2020-01-28 01:42:26 +0000

    Releave v0.1.0a1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = '2019-2020, Edwin Onuonga'
 author = 'Edwin Onuonga'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0a0'
+release = '0.1.0a1'
 
 # -- General configuration ---------------------------------------------------
 
